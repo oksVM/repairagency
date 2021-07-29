bc58d866a850d373decb445b6eb6590abc453149
oksana21moysyuk@gmail.com <oksana21moysyuk@gmail.com>
2021-07-29 18:04:43 +0300

    update settings

---
+++ src/main/java/com/example/repairagency/LocalizationController.java
@@ -0,0 +1,4 @@
+package com.example.repairagency;
+
+public class LocalizationController {
+}
---
+++ src/main/java/com/example/repairagency/RepairAgency.java
@@ -0,0 +1,13 @@
+package com.example.repairagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairAgency {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairAgency.class, args);
+    }
+
+}
