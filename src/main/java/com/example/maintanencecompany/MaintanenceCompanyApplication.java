dd1c45fdae199287444067cfd0f03a60fe2a9a2d
oksana21moysyuk@gmail.com <oksana21moysyuk@gmail.com>
2021-08-04 19:14:24 +0300

    security updated

--- src/main/java/com/example/maintanencecompany/MaintanenceCompanyApplication.java
+++ src/main/java/com/example/maintanencecompany/MaintanenceCompanyApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class git MaintanenceCompatyApplication {
+public class MaintanenceCompanyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MaintanenceCompatyApplication.class, args);
+
+        SpringApplication.run(MaintanenceCompanyApplication.class, args);
     }
 
 }
