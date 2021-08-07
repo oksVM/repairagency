cac711b23cc0244f97e9edba094447b7896de626
oksana21moysyuk@gmail.com <oksana21moysyuk@gmail.com>
2021-08-07 18:38:03 +0300

    show message about existen user login

--- src/main/java/com/example/repairagency/controller/AppUserRegistrationController.java
+++ src/main/java/com/example/repairagency/controller/AppUserRegistrationController.java
@@ -6,10 +6,13 @@ import com.example.repairagency.dto.UserAlreadyExistAuthenticationException;
 import com.example.repairagency.service.AppUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/registration")
@@ -41,16 +44,45 @@ public class AppUserRegistrationController {
 
     @PostMapping
     public String registerUserAccount(@ModelAttribute("user")
-                                              AppUserRegistrationDto appUserRegistrationDto){
+                                              @Valid AppUserRegistrationDto appUserRegistrationDto, BindingResult bindingResult, Model model){
 
     try {
     appUserService.save(appUserRegistrationDto);
     } catch (UserAlreadyExistAuthenticationException exception){
+        bindingResult.rejectValue("email", "userData.email","An account already exists for this email.");
+        model.addAttribute("registrationForm", appUserRegistrationDto);
+        return "registration";
 
-    return "redirect:/registration?userExist";
+    //return "redirect:/registration?userExist";
 
      }
         return "redirect:/registration?success";
 
     }
 }
+
+
+
+/*  @GetMapping
+    public String register(final Model model){
+        model.addAttribute("userData", new UserData());
+        return "account/register";
+    }
+
+    @PostMapping
+    public String userRegistration(final @Valid  UserData userData, final BindingResult bindingResult, final Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("registrationForm", userData);
+            return "account/register";
+        }
+        try {
+            userService.register(userData);
+        }catch (UserAlreadyExistException e){
+            bindingResult.rejectValue("email", "userData.email","An account already exists for this email.");
+            model.addAttribute("registrationForm", userData);
+            return "account/register";
+        }
+        model.addAttribute("registrationMsg", messageSource.getMessage("user.registration.verification.email.msg", null, LocaleContextHolder.getLocale()));
+        return "account/register";
+    }
+*/
